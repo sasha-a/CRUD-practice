8763826925f078355f13b87cf6e9e8dc2916c789
Andrea Frank & Aleksandra (Sasha) Ballard-Aktuganova <pair+andreafrank.sasha-a@devbootcamp.com>
2016-08-16 15:23:30 -0500

    Add validation for review rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,15 +8,14 @@ post '/restaurants/:id/reviews' do
   ensure_login!
   @restaurant = Restaurant.find_by(id: params[:id])
   @review = Review.new(params['review'])
-  # @review.user_id = current_user.id
-  # @restaurant.reviews << @review
-  # remove null false and move to inside if statement
-  if @review.save
-    @review.user_id = current_user.id
-    @restaurant.reviews << @review
-    @message = "Thank you for your review!"
-    redirect "/restaurants/#{@restaurant.id}"
-  else
-    "hello"
-  end
+
+    if @review.save
+      @review.user_id = current_user.id
+      @restaurant.reviews << @review
+      @message = "Thank you for your review!"
+      redirect "/restaurants/#{@restaurant.id}"
+    else
+      @errors = ["Please choose a rating"]
+      erb :"/reviews/new"
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
 
-  validates :points, presence: true
+  validates :points, presence: true, numericality: { greater_than: 0 }
 end
